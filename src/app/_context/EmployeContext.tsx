56890714ecb0f808a0483304360e3c459f071680
24LP2910 24LP2910 <24LP2910@admins-iMac.local>
2025-04-15 12:35:20 +0800

    employee context dutuu

--- src/app/_context/EmployeContext.tsx
+++ src/app/_context/EmployeContext.tsx
@@ -1,1 +1,13 @@
-// typeU
+import { createContext, ReactNode } from "react";
+
+
+type employeeContextType = {};
+const employeeContext = createContext<employeeContextType>({} as employeeContextType)
+const EmployeeProvider = ({children}:{children:ReactNode}) => {
+    return (
+        <employeeContext.Provider value ={{}}>{children}</employeeContext.Provider>
+    )
+}
+
+
+export  default EmployeeProvider;
