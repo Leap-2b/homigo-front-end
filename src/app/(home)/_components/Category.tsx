d25006d92d68f5cb5eccb243c80816da30e94907
24LP2910 24LP2910 <24LP2910@admins-iMac.local>
2025-04-28 12:35:53 +0800

    response bolson

--- src/app/(home)/_components/Category.tsx
+++ src/app/(home)/_components/Category.tsx
@@ -4,8 +4,8 @@ import EmployeDetail from "./Employe-detail";
 
 const Category = () => {
   return (
-    <div className="w-full m-auto flex flex-col items-center  bg-gray-100">
-      <div className="  items-center px-4 py-3 flex ">
+    <div className="w-full m-auto flex flex-col items-center  bg-gray-100 ">
+      <div className="w-full px-4 py-3 md:flex md:w-[80vw]">
         <CardSlider />
       </div>
       <EmployeDetail />
